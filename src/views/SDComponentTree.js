33d01dfc2bda09c6ff41e877b24b52c0a2fd48d7
raul mantilla <rmantilla26@gmail.com>
2023-08-04 00:35:47 -0500

    adding menu

--- src/views/SDComponentTree.js
+++ src/views/SDComponentTree.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const SDComponentTree = ({ component, selectedComponent, setSelectedComponent }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+  const componentRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
 
   const toggleExpanded = (e) => {
     e.stopPropagation();
@@ -10,17 +25,30 @@ const SDComponentTree = ({ component, selectedComponent, setSelectedComponent })
   };
 
   const selectComponent = () => {
+    setMenuOpen(false);
     setSelectedComponent(component);
   };
 
+  let menuStyle = { position: 'absolute', background: 'white', border: '1px solid #ccc' };
+
+  if (menuOpen && componentRef.current) {
+    const rect = componentRef.current.getBoundingClientRect();
+    menuStyle = {
+      ...menuStyle,
+      top: `${rect.bottom - 85}px`,
+      left: `${rect.left + 20}px`,
+    };
+  }
+
   const hasChildren = component.children && component.children.length > 0;
-  const childrenCount = hasChildren ? component.children.length : 0; // Cantidad de hijos
+  const childrenCount = hasChildren ? component.children.length : 0;
   const isObject = component.component_type === 'Object';
   const isSelected = selectedComponent && selectedComponent.id === component.id;
 
   return (
     <div>
       <div
+        ref={componentRef}
         onClick={selectComponent}
         className={`mb-1 p-2 ${isSelected ? 'border border-primary rounded' : ''}`}
         style={{
@@ -37,7 +65,41 @@ const SDComponentTree = ({ component, selectedComponent, setSelectedComponent })
         </span>
         <i className={`bi ${isObject ? 'bi-file-earmark' : 'bi-file-earmark-text'}`} />
         <span className="ms-2">{component.properties.component_type} {childrenCount > 0 && `[${childrenCount}]`}</span>
-        {/* Mostrar cantidad de hijos entre corchetes solo si es mayor a cero */}
+        {isSelected && (
+          <span onClick={(e) => e.stopPropagation()} style={{ cursor: 'pointer', marginLeft: '15px' }}>
+            <i className="bi bi-three-dots" onClick={() => setMenuOpen(!menuOpen)}></i>
+            {menuOpen && (
+              <div ref={menuRef} style={menuStyle}>
+                <div
+                  onClick={() => console.log('Embeber dentro de un Contenedor')}
+                  style={{ padding: '5px', cursor: 'pointer' }}
+                  onMouseOver={(e) => (e.target.style.background = '#f0f8ff')}
+                  onMouseOut={(e) => (e.target.style.background = 'transparent')}
+                >
+                  Embeber dentro de un Contenedor
+                </div>
+                <div
+                  onClick={() => console.log('Agregar un contenedor')}
+                  style={{ padding: '5px', cursor: 'pointer' }}
+                  onMouseOver={(e) => (e.target.style.background = '#f0f8ff')}
+                  onMouseOut={(e) => (e.target.style.background = 'transparent')}
+                >
+                  Agregar un contenedor
+                </div>
+                <hr style={{ margin: '5px 0' }} />
+                <div
+                  onClick={() => console.log('Agregar un objeto')}
+                  style={{ padding: '5px', cursor: 'pointer' }}
+                  onMouseOver={(e) => (e.target.style.background = '#f0f8ff')}
+                  onMouseOut={(e) => (e.target.style.background = 'transparent')}
+                >
+                  Agregar un objeto
+                </div>
+                {/* ... otras opciones ... */}
+              </div>
+            )}
+          </span>
+        )}
       </div>
       {isExpanded && hasChildren && (
         <div style={{ marginLeft: '20px' }}>
