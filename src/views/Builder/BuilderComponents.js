9f25d1ac797365850e6b19768c40a03ccc00dcac
raul mantilla <rmantilla26@gmail.com>
2023-08-16 18:59:32 -0500

    perfecto

--- src/views/Builder/BuilderComponents.js
+++ src/views/Builder/BuilderComponents.js
@@ -4,17 +4,21 @@ import Modal from 'react-bootstrap/Modal';
 import '../../css/Builder/BuilderComponents.css';
 
 function BuilderComponents({ setIsPropertiesOpen }) {
-  const [components, setComponents] = useState([
-    { id: 1, type: 'Texto', children: [], expanded: false },
-    { id: 2, type: 'Imagen', children: [], expanded: false },
-    { id: 3, type: 'Botón', children: [], expanded: false }
-  ]);
+  const [components, setComponents] = useState(initialComponents());
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [componentToDelete, setComponentToDelete] = useState(null);
   const [draggingComponent, setDraggingComponent] = useState(null);
 
-  const toggleExpanded = (targetId, currentComponents) => {
-    return currentComponents.map(component => {
+  function initialComponents() {
+    return [
+      { id: 1, type: 'Texto', children: [], expanded: false },
+      { id: 2, type: 'Imagen', children: [], expanded: false },
+      { id: 3, type: 'Botón', children: [], expanded: false }
+    ];
+  }
+
+  const toggleExpanded = (targetId, currentComponents) => 
+    currentComponents.map(component => {
       if (component.id === targetId) {
         return { ...component, expanded: !component.expanded };
       }
@@ -23,28 +27,25 @@ function BuilderComponents({ setIsPropertiesOpen }) {
       }
       return component;
     });
-  };
 
-  const handleToggleExpanded = (id) => {
+  const handleToggleExpanded = (id) => 
     setComponents(prevComponents => toggleExpanded(id, prevComponents));
-  };
 
-  const deleteComponentRecursive = (idToDelete, currentComponents) => {
-    return currentComponents.filter(comp => comp.id !== idToDelete).map(component => {
+  const deleteComponentRecursive = (idToDelete, currentComponents) => 
+    currentComponents.filter(comp => comp.id !== idToDelete).map(component => {
       if (component.children.length > 0) {
         component.children = deleteComponentRecursive(idToDelete, component.children);
       }
       return component;
     });
-  };
 
   const deleteComponent = (compToDelete) => {
     setComponents(prevComponents => deleteComponentRecursive(compToDelete.id, prevComponents));
     setShowDeleteModal(false);
   };
 
-  const addComponentChildRecursive = (parentId, currentComponents, childComponent) => {
-    return currentComponents.map(component => {
+  const addComponentChildRecursive = (parentId, currentComponents, childComponent) => 
+    currentComponents.map(component => {
       if (component.id === parentId) {
         return { ...component, children: [...component.children, childComponent] };
       }
@@ -53,7 +54,6 @@ function BuilderComponents({ setIsPropertiesOpen }) {
       }
       return component;
     });
-  };
 
   const addComponentChild = (parentId) => {
     const child = { id: Date.now(), type: `Hijo`, children: [], expanded: false };
@@ -66,43 +66,34 @@ function BuilderComponents({ setIsPropertiesOpen }) {
     setDraggingComponent(component);
   };
 
-  const handleDragEnter = (event, componentId) => {
+  const handleDragEnterLeaveOrOver = (event, componentId, isOver = false) => {
     event.stopPropagation();
     if (draggingComponent.id !== componentId && !isDescendant(draggingComponent, componentId)) {
+      if (isOver) {
+        event.preventDefault();
         event.currentTarget.classList.add('ready-for-drop');
-    }
-  };
-
-  const handleDragOver = (event, componentId) => {
-    event.preventDefault();
-    event.stopPropagation();
-    if (draggingComponent.id !== componentId && !isDescendant(draggingComponent, componentId)) {
-        event.currentTarget.classList.add('ready-for-drop');
-    } else {
+      } else {
         event.currentTarget.classList.remove('ready-for-drop');
+      }
     }
   };
 
-  const handleDragLeave = (event) => {
-    event.stopPropagation();
-    event.currentTarget.classList.remove('ready-for-drop');
-  };
-
-  const removeComponent = (componentId, currentComponents) => {
-    return currentComponents.reduce((acc, component) => {
-      if (component.id === componentId) return acc;
-      if (component.children.length) {
-        component.children = removeComponent(componentId, component.children);
+  const removeComponent = (componentId, currentComponents) => 
+    currentComponents.reduce((acc, component) => {
+      if (component.id !== componentId) {
+        if (component.children.length) {
+          component.children = removeComponent(componentId, component.children);
+        }
+        acc.push(component);
       }
-      acc.push(component);
       return acc;
     }, []);
-  };
 
   const isDescendant = (parentComponent, targetId) => {
     for (let child of parentComponent.children) {
-      if (child.id === targetId) return true;
-      if (isDescendant(child, targetId)) return true;
+      if (child.id === targetId || isDescendant(child, targetId)) {
+        return true;
+      }
     }
     return false;
   };
@@ -110,39 +101,39 @@ function BuilderComponents({ setIsPropertiesOpen }) {
   const handleDrop = (event, parentId) => {
     event.preventDefault();
     event.stopPropagation();
-
-    if (draggingComponent.id === parentId) {
-      setDraggingComponent(null);
-      return;
-    }
-
-    if (isDescendant(draggingComponent, parentId)) {
-        setDraggingComponent(null);
-        return;
-    }
-
-    if (draggingComponent) {
-        const updatedComponents = removeComponent(draggingComponent.id, components);
-        const newComponents = addComponentChildRecursive(parentId, updatedComponents, draggingComponent);
-        setComponents(newComponents);
-        setDraggingComponent(null);
+    if (draggingComponent && draggingComponent.id !== parentId && !isDescendant(draggingComponent, parentId)) {
+      const updatedComponents = removeComponent(draggingComponent.id, components);
+      const newComponents = addComponentChildRecursive(parentId, updatedComponents, draggingComponent);
+      setComponents(newComponents);
     }
     event.currentTarget.classList.remove('ready-for-drop');
+    setDraggingComponent(null);
   };
 
-  const renderComponentList = (compArray, parentId = null) => {
-    return compArray.map(comp => (
-      <div 
-        
-      >
-        <div key={comp.id} 
-        draggable={comp.id !== parentId}
-        onDragStart={(event) => handleDragStart(event, comp)}
-        onDragOver={(event) => handleDragOver(event, comp.id)}
-        onDrop={(event) => handleDrop(event, comp.id)}
-        onDragEnter={(event) => handleDragEnter(event, comp.id)}
-        onDragLeave={handleDragLeave}
-        className={`${comp === draggingComponent ? 'dragging' : ''}`} className={`component-item ${draggingComponent && comp.id !== draggingComponent.id ? 'drop-target' : ''}`} 
+  const handleDragEnd = () => {
+  // Limpiar el estado del componente que se está arrastrando.
+  setDraggingComponent(null);
+
+  // Limpiar las clases de cualquier componente que se haya resaltado como destino de soltado.
+  const dropTargets = document.querySelectorAll('.drop-target');
+  dropTargets.forEach(target => {
+    target.classList.remove('drop-target', 'ready-for-drop');
+  });
+};
+
+
+  const renderComponentList = (compArray, parentId = null) => 
+    compArray.map(comp => (
+      <div key={comp.id}>
+        <div
+          draggable={comp.id !== parentId}
+          onDragStart={(e) => handleDragStart(e, comp)}
+          onDragEnd={handleDragEnd}  // Aquí está el nuevo controlador
+          onDragOver={(e) => handleDragEnterLeaveOrOver(e, comp.id, true)}
+          onDrop={(e) => handleDrop(e, comp.id)}
+          onDragEnter={(e) => handleDragEnterLeaveOrOver(e, comp.id)}
+          onDragLeave={handleDragEnterLeaveOrOver}
+          className={`component-item ${comp === draggingComponent ? 'dragging' : ''} ${draggingComponent && comp.id !== draggingComponent.id ? 'drop-target' : ''}`}
           onClick={() => setIsPropertiesOpen(true)}>
           <span className="toggle-btn" onClick={(e) => { e.stopPropagation(); handleToggleExpanded(comp.id); }}>{comp.expanded ? '-' : '+'}</span>
           <span>{comp.type}</span>
@@ -152,26 +143,25 @@ function BuilderComponents({ setIsPropertiesOpen }) {
         {comp.expanded && comp.children.length > 0 && <div className="component-children">{renderComponentList(comp.children, comp.id)}</div>}
       </div>
     ));
-  };
 
-    return (
-        <div className="buildercomponents">
-            <h2>Componentes</h2>
-            {renderComponentList(components)}
-            {showDeleteModal && (
-                <Modal show={showDeleteModal}>
-                    <Modal.Header>
-                        <Modal.Title>Eliminar Componente</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>¿Desea eliminar este componente?</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>Cancelar</Button>
-                        <Button variant="danger" onClick={() => deleteComponent(componentToDelete)}>Eliminar</Button>
-                    </Modal.Footer>
-                </Modal>
-            )}
-        </div>
-    );
+  return (
+    <div className="buildercomponents">
+      <h2>Componentes</h2>
+      {renderComponentList(components)}
+      {showDeleteModal && (
+        <Modal show={showDeleteModal}>
+          <Modal.Header>
+            <Modal.Title>Eliminar Componente</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>¿Desea eliminar este componente?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>Cancelar</Button>
+            <Button variant="danger" onClick={() => deleteComponent(componentToDelete)}>Eliminar</Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
+  );
 }
 
 export default BuilderComponents;
