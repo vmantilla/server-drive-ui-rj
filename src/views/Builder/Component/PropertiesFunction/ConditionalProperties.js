2e7612a5ae6490e7cac54103f070c1e42c9094a8
RavitDev2023 <rmantilla26@gmail.com>
2023-10-25 17:46:28 -0500

    manipulando el condicional if else

--- src/views/Builder/Component/PropertiesFunction/ConditionalProperties.js
+++ src/views/Builder/Component/PropertiesFunction/ConditionalProperties.js
@@ -1,13 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import '../../../../css/Builder/Component/PropertiesFunction/ConditionalProperties.css';
 
+function Group({ group, index, handleGroupChange, handleRemoveGroup }) {
+    const [conditions, setConditions] = useState(group.conditions || []);
+
+    useEffect(() => {
+        handleGroupChange(index, 'conditions', conditions);
+    }, [conditions]);
+
+    const handleConditionChange = (condIndex, key, value) => {
+        const newConditions = [...conditions];
+        newConditions[condIndex] = { ...newConditions[condIndex], [key]: value };
+        setConditions(newConditions);
+    };
+
+    const handleRemoveCondition = (condIndex) => {
+        const newConditions = conditions.filter((_, i) => i !== condIndex);
+        setConditions(newConditions);
+    };
+
+    const handleAddCondition = () => {
+        setConditions([...conditions, { operator: "==", values: ["", ""] }]);
+    };
+
+    return (
+        <div className="conditional-group" style={{ border: '1px solid #000', padding: '10px', marginBottom: '10px' }}>
+            <div className="conditional-group-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            </div>
+            <ConditionalProperties 
+                conditions={conditions} 
+                handleConditionsChange={setConditions} 
+                handleConditionChange={handleConditionChange}
+                handleRemoveCondition={handleRemoveCondition}
+                handleAddCondition={handleAddCondition}
+            />
+        </div>
+    );
+}
+
 function Condition({ condition, index, handleConditionChange, handleRemoveCondition }) {
     return (
         <div className="conditional-properties-row">
             <div className="conditional-property">
                 <label>Condition {index + 1}</label>
                 <div className="input-wrapper">
-                    <select value={condition.type} onChange={(e) => handleConditionChange(index, 'type', e.target.value)}>
+                    <select value={condition.operator} onChange={(e) => handleConditionChange(index, 'operator', e.target.value)}>
                         <option value="==">Equal to (==)</option>
                         <option value="!=">Not equal to (!=)</option>
                         <option value="!">NOT (!)</option>
@@ -25,7 +62,7 @@ function Condition({ condition, index, handleConditionChange, handleRemoveCondit
                     />
                 </div>
             </div>
-            {condition.type !== "!" && (
+            {condition.operator !== "!" && (
                 <div className="conditional-property">
                     <label>Value:</label>
                     <div className="input-wrapper">
@@ -37,29 +74,24 @@ function Condition({ condition, index, handleConditionChange, handleRemoveCondit
                     </div>
                 </div>
             )}
-            <button onClick={() => handleRemoveCondition(index)}>Remove</button>
         </div>
     );
 }
 
 function ConditionalProperties({ property, handlePropertyChange }) {
-    const [conditions, setConditions] = useState(property.data.conditions || []);
+    const [conditions, setConditions] = useState(
+        property && property.data && property.data.conditions ? property.data.conditions : []
+    );
 
     useEffect(() => {
+        if (typeof handlePropertyChange === 'function') {
         handlePropertyChange('conditions', conditions);
-    }, [conditions]);
+    }
+    }, [conditions, handlePropertyChange]);
 
     const handleConditionChange = (index, key, value) => {
         const newConditions = [...conditions];
-        if (key === 'type' && value === '!') {
-            // If the type is changed to NOT, keep only the first value
-            newConditions[index] = { ...newConditions[index], [key]: value, values: [newConditions[index].values[0]] };
-        } else if (key === 'values' && newConditions[index].type === '!' && value.length > 1) {
-            // If the type is NOT, and values are being updated, keep only the first value
-            newConditions[index] = { ...newConditions[index], [key]: [value[0]] };
-        } else {
-            newConditions[index] = { ...newConditions[index], [key]: value };
-        }
+        newConditions[index] = { ...newConditions[index], [key]: value };
         setConditions(newConditions);
     };
 
@@ -69,21 +101,47 @@ function ConditionalProperties({ property, handlePropertyChange }) {
     };
 
     const handleAddCondition = () => {
-        setConditions([...conditions, { type: "==", values: ["", ""] }]);
+        setConditions([...conditions, { operator: "==", values: ["", ""] }]);
+    };
+
+     const handleAddGroup = () => {
+        setConditions([...conditions, { operator: "AND", conditions: [] }]);
     };
 
     return (
         <div className="conditional-properties">
-            {conditions.map((condition, index) => (
-                <Condition
-                    key={index}
-                    index={index}
-                    condition={condition}
-                    handleConditionChange={handleConditionChange}
-                    handleRemoveCondition={handleRemoveCondition}
-                />
-            ))}
+            {conditions.map((condition, index) => 
+                <>
+                    {index !== 0 && (
+                        <div className="conditional-property-mini-header">
+                            <select className="conditional-mini-header-dropdown" onChange={(e) => {/* Tu función de manejo aquí */}}>
+                                <option value="AND">AND</option>
+                                <option value="OR">OR</option>
+                            </select>
+                            <button className="conditional-delete-property-button" onClick={() => handleRemoveCondition(index)}>-</button>
+                        </div>
+                    )}
+                    {condition.conditions ? (
+                        <Group 
+                            key={index} 
+                            index={index} 
+                            group={condition} 
+                            handleGroupChange={handleConditionChange} 
+                            handleRemoveGroup={handleRemoveCondition} 
+                        />
+                    ) : (
+                        <Condition 
+                            key={index} 
+                            index={index} 
+                            condition={condition} 
+                            handleConditionChange={handleConditionChange} 
+                            handleRemoveCondition={handleRemoveCondition}
+                        />
+                    )}
+                </>
+            )}
             <button onClick={handleAddCondition}>Add Condition</button>
+            <button onClick={handleAddGroup}>Add Group</button>
         </div>
     );
 }
