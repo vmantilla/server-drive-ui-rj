0c6cd5701f85609cbca05997c33209d3751065db
Raul Mantilla <rmantilla26@gmail.com>
2024-02-01 15:05:55 -0500

    ok completa sugerencias

--- src/views/Builder/Preview/PreviewExperience.js
+++ src/views/Builder/Preview/PreviewExperience.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../css/Builder/Preview/PreviewExperience.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Modal from 'react-bootstrap/Modal';
@@ -11,135 +11,132 @@ import {
 } from '../../api';
 
 const PreviewExperience = ({ selectedPreview }) => {
-  const [useCases, setUseCases] = useState([]);
-  const [editingUseCaseId, setEditingUseCaseId] = useState(null);
-  const [editingUseCaseDesc, setEditingUseCaseDesc] = useState('');
-  const [newUseCaseDesc, setNewUseCaseDesc] = useState('');
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [deletingUseCaseId, setDeletingUseCaseId] = useState(null);
-  const [showAddModal, setShowAddModal] = useState(false);
-  const textAreaRef = useRef(null);
+  const [groupedUseCases, setGroupedUseCases] = useState({});
+  const [modalContent, setModalContent] = useState({ description: '', actor: '', editing: false, id: null });
+  const [showModal, setShowModal] = useState(false);
+  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+  const [useCaseToDelete, setUseCaseToDelete] = useState(null);
 
   useEffect(() => {
-    getUseCasesFromAPI(selectedPreview).then((data) => setUseCases(data));
+    getUseCasesFromAPI(selectedPreview).then((data) => {
+      const grouped = data.reduce((acc, curr) => {
+        (acc[curr.actor] = acc[curr.actor] || []).push(curr);
+        return acc;
+      }, {});
+      setGroupedUseCases(grouped);
+    });
   }, [selectedPreview]);
 
-  const handleTextAreaChange = (e) => {
-    e.target.style.height = 'inherit';
-    e.target.style.height = `${e.target.scrollHeight}px`;
-    setEditingUseCaseDesc(e.target.value);
+  const handleModalShow = (actor, useCase = null) => {
+    if (useCase) {
+      setModalContent({ ...useCase, actor, editing: true });
+    } else {
+      setModalContent({ description: '', actor, editing: false, id: null });
+    }
+    setShowModal(true);
   };
 
-  const handleAddUseCase = () => {
-    addUseCaseToAPI(selectedPreview, { description: newUseCaseDesc })
-      .then((newUseCase) => {
-        setUseCases([...useCases, newUseCase]);
-        setNewUseCaseDesc('');
-        setShowAddModal(false);
-      });
+  const handleModalChange = (e) => {
+    setModalContent({ ...modalContent, description: e.target.value });
   };
 
-  const handleEditUseCase = (useCaseId, useCaseDesc) => {
-    setEditingUseCaseId(useCaseId);
-    setEditingUseCaseDesc(useCaseDesc);
+  const handleModalSave = () => {
+    const { editing, id, actor, description } = modalContent;
+    if (editing) {
+      editUseCaseInAPI(selectedPreview, id, { description })
+        .then(() => {
+          const updatedGroupedUseCases = { ...groupedUseCases };
+          updatedGroupedUseCases[actor] = updatedGroupedUseCases[actor].map(useCase =>
+            useCase.id === id ? { ...useCase, description } : useCase
+          );
+          setGroupedUseCases(updatedGroupedUseCases);
+        });
+    } else {
+      addUseCaseToAPI(selectedPreview, { description, actor })
+        .then((addedUseCase) => {
+          const updatedGroupedUseCases = { ...groupedUseCases };
+          const actorUseCases = updatedGroupedUseCases[actor] || [];
+          updatedGroupedUseCases[actor] = [...actorUseCases, addedUseCase];
+          setGroupedUseCases(updatedGroupedUseCases);
+        });
+    }
+    setShowModal(false);
   };
 
-  const saveEditUseCase = () => {
-    editUseCaseInAPI(selectedPreview, editingUseCaseId, { description: editingUseCaseDesc })
-      .then((updatedUseCase) => {
-        setUseCases(useCases.map(useCase => useCase.id === editingUseCaseId ? updatedUseCase : useCase));
-        setEditingUseCaseId(null);
-      });
-  };
-
-  const handleDeleteUseCase = (useCaseId) => {
-    setDeletingUseCaseId(useCaseId);
-    setShowDeleteModal(true);
+  const handleDeleteConfirmation = (actor, useCaseId) => {
+    setUseCaseToDelete({ actor, useCaseId });
+    setShowConfirmDelete(true);
   };
 
   const confirmDeleteUseCase = () => {
-    deleteUseCaseFromAPI(selectedPreview, deletingUseCaseId)
-      .then(() => {
-        setUseCases(useCases.filter(useCase => useCase.id !== deletingUseCaseId));
-        setShowDeleteModal(false);
-        setDeletingUseCaseId(null);
-      });
+    if (useCaseToDelete) {
+      deleteUseCaseFromAPI(selectedPreview, useCaseToDelete.useCaseId)
+        .then(() => {
+          const updatedGroupedUseCases = { ...groupedUseCases };
+          updatedGroupedUseCases[useCaseToDelete.actor] = updatedGroupedUseCases[useCaseToDelete.actor].filter(useCase => useCase.id !== useCaseToDelete.useCaseId);
+          setGroupedUseCases(updatedGroupedUseCases);
+          setShowConfirmDelete(false);
+        });
+    }
   };
 
   return (
-    <div className="preview-experience">
-      <h2>Características de la Pantalla</h2>
-      <button onClick={() => setShowAddModal(true)}>Agregar Característica</button>
-      <ul>
-        {useCases.map((useCase, index) => (
-          <li key={index}>
-            <div className="suggestion-content">
-              {editingUseCaseId === useCase.id ? (
-                <textarea
-                  ref={textAreaRef}
-                  value={editingUseCaseDesc}
-                  onChange={handleTextAreaChange}
-                />
-              ) : (
-                <p>{useCase.description}</p>
-              )}
-            </div>
-            <div className="workspace-actions">
-              {editingUseCaseId === useCase.id ? (
-                <>
-                  <i className="bi bi-check-lg" onClick={saveEditUseCase}></i>
-                  <i className="bi bi-x-lg" onClick={() => setEditingUseCaseId(null)}></i>
-                </>
-              ) : (
-                <>
-                  <i className="bi bi-pen" onClick={() => handleEditUseCase(useCase.id, useCase.description)}></i>
-                  <i className="bi bi-trash" onClick={() => handleDeleteUseCase(useCase.id)}></i>
-                </>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
-
-      {/* Modal para agregar nueva característica */}
-      <Modal show={showAddModal} onHide={() => setShowAddModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Agregar Característica</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <textarea
-            value={newUseCaseDesc}
-            onChange={(e) => setNewUseCaseDesc(e.target.value)}
-            placeholder="Descripción de la Característica"
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowAddModal(false)}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={handleAddUseCase}>
-            Agregar
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-      {/* Modal para confirmar eliminación */}
-      <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirmar Eliminación</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          ¿Estás seguro de que deseas eliminar esta característica?
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>
-            Cancelar
-          </Button>
-          <Button variant="danger" onClick={confirmDeleteUseCase}>
-            Eliminar
-          </Button>
-        </Modal.Footer>
-      </Modal>
+     <div className="preview-experience-container">
+      <h2 className="preview-experience-container-title">Características de la Pantalla</h2>
+      <div className="preview-experience">
+      {Object.entries(groupedUseCases).map(([actor, useCases]) => (
+        <div key={actor}>
+          <header className="preview-experience-header">
+            <span className="preview-experience-title">{actor.charAt(0).toUpperCase() + actor.slice(1)} Actions</span>
+            <button className="add-use-case-button" onClick={() => handleModalShow(actor)}>+</button>
+          </header>
+          <ul>
+            {useCases.map((useCase) => (
+              <li key={useCase.id} className="use-case-item">
+                <div onClick={() => handleModalShow(actor, useCase)}>
+                  {useCase.description}
+                </div>
+                <button className="delete-use-case-button" onClick={() => handleDeleteConfirmation(actor, useCase.id)}>
+                  <i className="bi bi-x"></i>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      {showModal && (
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>{modalContent.editing ? 'Editar' : 'Agregar'} Característica</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <textarea
+              value={modalContent.description}
+              onChange={handleModalChange}
+              placeholder="Descripción de la característica"
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowModal(false)}>Cancelar</Button>
+            <Button variant="primary" onClick={handleModalSave}>{modalContent.editing ? 'Guardar Cambios' : 'Agregar'}</Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+      {showConfirmDelete && (
+        <Modal show={showConfirmDelete} onHide={() => setShowConfirmDelete(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirmar Eliminación</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            ¿Estás seguro de que deseas eliminar esta característica?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowConfirmDelete(false)}>Cancelar</Button>
+            <Button variant="danger" onClick={confirmDeleteUseCase}>Eliminar</Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
     </div>
   );
 };
